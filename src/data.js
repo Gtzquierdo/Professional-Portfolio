b761d9db0f7acdf86620e951026b8a2411206c05
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-08-04 22:11:18 -0400

    title input

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    title: "Denver Nuggets Trivia Night!",
+    title: "Hundo: A Coding App",
     subtitle: "Javascript",
     description:
       "Live life one loop at a time. A javascript coding exercise using loops. Write a program that displays the numbers from 0 to 100. Display the even numbers in BOLD.",
