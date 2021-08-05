0207cc2269b1ee99a7b7a9dceadb12697b2d0be3
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-08-04 22:10:28 -0400

    img
    project img update

--- src/data.js
+++ src/data.js
@@ -3,9 +3,9 @@ export const projects = [
     title: "Denver Nuggets Trivia Night!",
     subtitle: "Javascript",
     description:
-      "A fun basic 4 question Trivia Game. The theme is based around my favorite NBA team and Colorado sports team....The Denver Nuggets",
-    image: "https://upload.wikimedia.org/wikipedia/en/thumb/7/76/Denver_Nuggets.svg/1200px-Denver_Nuggets.svg.png",
-    link: "https://gtzquierdo.github.io/TriviaGame/",
+      "Live life one loop at a time. A javascript coding exercise using loops. Write a program that displays the numbers from 0 to 100. Display the even numbers in BOLD.",
+    image: "../public/Project_Img.svg",
+    link: "https://gtzquierdo.github.io/Hundo/index.html",
   },
   {
     title: "Train Schedule",
