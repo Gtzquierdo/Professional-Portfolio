ea388316650de861e433e152a0ad01267d8a6998
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2021-08-13 14:24:53 -0400

    Correction for build

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Fizz Buzz: A Coding Project",
     subtitle: "Javascript",
     description:
-      "A javascript coding exercise using loops. Write a program that prints the numbers from 1 to 100 and for multiples of '3' print "Fizz" instead of the number and for the multiples of '5' print "Buzz." If a multiple of both display "FizzBuzz."",
+      "A javascript coding exercise using loops. Write a program that prints the numbers from 1 to 100 and for multiples of '3' print Fizz instead of the number and for the multiples of '5' print Buzz. If a multiple of both display FizzBuzz.",
     image: "./fizzBuzz.png",
     link: "https://gtzquierdo.github.io/FizzBuzz/",
   },
