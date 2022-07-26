297c9fe9914de7a9128ee946c54d40b3c08c3809
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2022-07-26 09:18:17 -0400

    updating imgs

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
     subtitle: "Javascript",
     description:
       "A javascript coding exercise using loops. Write a program that prints the numbers from 1 to 100 and for multiples of '3' print Fizz instead of the number and for the multiples of '5' print Buzz. If a multiple of both display FizzBuzz.",
-    image: "./imgs/fizzBuzz.png",
+    image: "../public/fizzBuzz.png",
     link: "https://gtzquierdo.github.io/FizzBuzz/",
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     subtitle: "React, Next.js, Sanity.io, Stripe",
     description:
       "Built for client(In Progress): A fully responsive Modern Full Stack Ecommerce application with Payments functionality using Stripe API. The application is built using React, Next.js, Sanity.io, and Stripe API. I learned how to use Stripe API to create a payment form and how to use Sanity.io to create a content management system. I also learned how to use Next.js to create a server side rendered application.",
-    image: "../public/imgs/greatFind.png",
+    image: "../public/greatFind.png",
     link: "https://the-great-find.vercel.app/",
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "HTML, CSS, Javascript, Bootstrap 5",
     description:
       "There and back again. A Javascript coding exercise using arrays. Write a program that display a string in reverse.",
-    image: "./imgs/rewind.png",
+    image: "../public/rewind.png",
     link: "https://gtzquierdo.github.io/Rewind/",
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     subtitle: "Javascript, MomentJS, and Firebase",
     description:
       "Train-Time App where I used Firebase to store data as well as using MomentJS",
-    image: "./imgs/train.png",
+    image: "../public/train.png",
     link: "https://gtzquierdo.github.io/Train-Time/",
   },
 ];
